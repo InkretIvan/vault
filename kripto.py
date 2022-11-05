aa18a9688afc10d5df54a8e1752bbbefd70ef0d1
InkretIvan <ivaninkret7@gmail.com>
2022-11-05 23:58:29 +0100

    k

---
+++ kripto.py
@@ -0,0 +1,8 @@
+from ecdsa import SigningKey
+
+sk = SigningKey.generate() # uses NIST192p
+vk = sk.verifying_key
+signature = sk.sign(b"message")
+assert vk.verify(signature, b"message")
+
+print("hello")
