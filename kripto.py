a934e9933c15909d717a0c2825d7605176bffdbb
InkretIvan <ivaninkret7@gmail.com>
2022-11-07 15:23:05 +0100

    pocetak klase

--- kripto.py
+++ kripto.py
@@ -3,6 +3,30 @@ from ecdsa import SigningKey
 sk = SigningKey.generate() # uses NIST192p
 vk = sk.verifying_key
 signature = sk.sign(b"message")
+#print(signature.decode())
 assert vk.verify(signature, b"message")
 
-print("hello")
+
+class Kripto:
+    def __init__(self):
+        self.sk = SigningKey.generate() # uses NIST192p
+        self.vk = sk.verifying_key
+
+    def sign(self,message):   #ocekujem string
+        byteString = bytes(message, 'utf-8')
+        signature = sk.sign(byteString)
+        self.signature=signature
+        return signature
+
+    def verify(self, signature, message):
+        byteString = bytes(message, 'utf-8')
+        print(vk.verify(signature, byteString))
+
+k1=Kripto()
+sig=k1.sign("bok")
+print(sig)
+k1.verify(sig,"bok")
+
+
+
+
